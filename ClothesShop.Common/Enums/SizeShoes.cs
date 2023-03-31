295ed18de3748f4bfd96c2eabb717705ee8221f2
Ludovit <ludsce002@gmail.com>
2023-03-31 17:18:39 +0200

    create enums data model

---
+++ ClothesShop.Common/Enums/SizeShoes.cs
@@ -0,0 +1,17 @@
+﻿namespace ClothesShopWebAPI.Enums
+{
+    public enum SizeShoes
+    {
+        U35 = 35,
+        U36 = 36,
+        U37 = 37,
+        U38 = 38,
+        U39 = 39,
+        U40 = 40,
+        U41 = 41,
+        U42 = 42,
+        U43 = 43,
+        U44 = 44,
+        U45 = 45
+    }
+}
