a6c918277d0b546286e9421473d4fd0954484525
tomaskotian <11tomas16@gmail.com>
2023-05-07 23:17:31 +0200

    delete test EF worker

--- ClothesShopWebAPI/Migrations/20230507211655_init.Designer.cs
+++ ClothesShopWebAPI/Migrations/20230507211655_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClothesShopWebAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20230507211323_init")]
+    [Migration("20230507211655_init")]
     partial class init
     {
         /// <inheritdoc />
